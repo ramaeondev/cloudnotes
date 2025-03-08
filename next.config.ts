afba8c384409803e00e8cbe8a0b330992d9a667c
ramaeon <Rama.eon@gmail.com>
2025-03-08 15:47:14 +0530

    feat: AWS S3 Deployment

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,7 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-  },
-  // Disable API routes
-  experimental: {
-    appDir: true,
-  },
-  // Optional: Disable API routes warning
-  api: {
-    externalResolver: true,
-  },
+  }
 }
 
 module.exports = nextConfig
